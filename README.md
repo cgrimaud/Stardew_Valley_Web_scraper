b2a85901ca9ca7e569b275b6e1f04615f580af2a
Christy Grimaud <christy.grimaud@gmail.com>
2020-05-21 23:21:51 -0400

    updated webscraper to pull data from all tables on page

---
+++ README.md
@@ -0,0 +1,3 @@
+# Stardew Valley Webscraper
+
+This is a project written in Python. I'm pulling data from the Stardew Valley Wiki website 
--- webscraper.py
+++ webscraper.py
@@ -5,16 +5,20 @@ url_minerals = "https://stardewvalleywiki.com/Minerals"
 response = requests.get(url_minerals)
 content = BeautifulSoup(response.content, "html.parser")
 
-mineral_table = content.find("table", {"id":"calendartable"})
+# mineral_table = content.find("table", {"id":"calendartable"})
+# all_links = mineral_table.findAll('a')
+# all_minerals = []
+# for link in all_links:
+#   all_minerals.append(link.get('title'))
+# print(all_minerals)
 
-all_links = mineral_table.findAll('a')
+content_div = content.find('div', {'class': 'mw-content-ltr'})
+content_tables = content_div.find_all('td')
+tables = []
+for table in content_tables:
+    tables.append(table.text)
+#content_body_contents = content_table_body.find_all('td')
 
-all_minerals = []
-for link in all_links:
-  all_minerals.append(link.get('title'))
+print (tables)
 
-print(content)
-
-
-
-
+#print(all_tables)
